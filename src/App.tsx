d6674b0cc6a5ba8c940300dba7a0efffdb683a74
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-02-04 10:46:39 +0300

    team work about promise

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Lesson1 from './lessons/lesson1/lesson1';
+import Lesson4 from "./lessons/lesson4/Lesson4";
 // import './lessons/lesson2/lesson2';
 // import Lesson3 from './lessons/lesson3/Lesson3';
 // import Lesson4 from './lessons/lesson4/Lesson4';
@@ -12,9 +12,10 @@ import Lesson1 from './lessons/lesson1/lesson1';
 function App() {
     return (
         <div className="container">
-            <Lesson1 />
-            {/*<Lesson3 />*/}
-            {/*<Lesson4 />*/}
+            {/*<Lesson1 />*/}
+            {/*/!*<Lesson3 />*!/*/}
+            <Lesson4/>
+            {/*<Lesson8/>*/}
         </div>
     );
 }
--- src/lessons/lesson4/Lesson4.tsx
+++ src/lessons/lesson4/Lesson4.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './lesson_4';
+import {CreatePromise, RejectPromise, ResolvePromise} from "./lesson_4";
 
 const Lesson4 = () => {
+
+
     return (
         <div>
-            <button id='btn-create-promise'>Create Promise</button>
-            <button id='btn-resolve-promise'>Resolve Promise</button>
-            <button id='btn-reject-promise'>Reject Promise</button>
+            <button onClick={CreatePromise} id='btn-create-promise'>Create Promise
+            </button>
+            <button onClick={ResolvePromise} id='btn-resolve-promise'>Resolve Promise
+            </button>
+            <button onClick={RejectPromise} id='btn-reject-promise'>Reject Promise
+            </button>
         </div>
     );
 }
