39b901a735646a3bf774476340066aae3bf8a4d6
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-02-06 22:04:47 +0300

    team work about this, call, apply

--- src/lessons/lesson5/HomeWork5.tsx
+++ src/lessons/lesson5/HomeWork5.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export {}
 
+export const HomeWork5 = () => {
 
 // Keyword - this
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/this
@@ -36,8 +37,11 @@ export {}
 // }
 //
 // function greeting  (this: someObjType)  {
-//     let {name, age} = this
+//
+//     let {age,name}=this
+//
 //     return `My name is ${name}. I am ${age}`
+//
 // }
 //
 // const someObj:someObjType = {
@@ -53,7 +57,7 @@ export {}
 // const someObj2:someObjType = {
 //     name: 'Eugene',
 //     age: 32,
-//     greeting: ()=>{
+//     greeting(){
 //         return `My name is ${this.name}. I am ${this.age}`
 //     }
 // }
@@ -61,13 +65,11 @@ export {}
 // console.log(someObj2.greeting())
 
 
-//можем ли мы создать стрелочную функцию отдельно и присвоить свойству объекта ?
+// можем ли мы создать стрелочную функцию отдельно и присвоить свойству объекта ?
 
 // const greeting2 = () => {
 //     return `My name is ${name}. I am ${age}`
 // }
-
-
 
 
 // Task 02
@@ -119,15 +121,84 @@ export {}
 // Task 03
 // переделайте код из Task 02, что бы сработал следующий код:
 // counter.setCurrentCount(10).increment().increment().increment().decrement().getCurrentCount() // 12
+//
+// type newObjType = {
+//     counter: number
+//     getCurrentCount: ()=>void
+//     increment: ()=>void
+//     decrement: ()=>void
+//     setCurrentCount: (newCounter:number)=>void
+//     restCurrentCount: ()=>void
+// }
+//
+// let newObj:newObjType = {
+//     counter: 0,
+//     getCurrentCount ():newObjType{
+//         console.log(this.counter)
+//         return this
+//     },
+//     increment():newObjType {
+//         this.counter = this.counter + 1
+//         return this
+//     },
+//     decrement():newObjType{
+//         this.counter = this.counter - 1
+//         return this
+//     },
+//     setCurrentCount(newCounter):newObjType{
+//         this.counter = Number(newCounter)
+//         return this
+//     },
+//     restCurrentCount():newObjType{
+//         this.counter = 0
+//         return this
+//     },
+// }
+// // как типизировать возвращаемый объект в function (){}
+// // @ts-ignore
+// window.counter = 10
+// // @ts-ignore
+// window.newObj = newObj
+
 
 // Task 04
 // Написать функцию конструктор myFirstConstructorFunc которая принимает 2 параметра name и age и возвращает объект
 // у которого будут эти свойства и метод greeting из Task 01
+// type someObjType = {
+//     name: string;
+//     age: number;
+//     greeting: ()=>string
+// }
+//
+// function greeting  (this: someObjType)  {
+//     return `My name is ${this.name}. I am ${this.age}`
+// }
+//
+// type MyFirstConstructorFuncType = (name:string, age: number)=>someObjType
+//
+// let  MyFirstConstructorFunc:MyFirstConstructorFuncType = (name, age)=>  {
+//   return {
+//       name: name,
+//       age: age,
+//       greeting: greeting
+//   }
+// }
+//
+//     // @ts-ignore
+//     let newObj:any = new MyFirstConstructorFunc('Andrew', 28)
+
 
 // Task 05 есть 2 объекта One и Two. С помощью bind и метода sayHello заставьте поздороваться объект One
 //
 // let One = {name: 'One'};
-// let Two = {name: 'Two', sayHello: function() {console.log(`Hello, my name is ${this.name}`)}};
+// let Two = {
+//     name: 'Two',
+//     sayHello: function() {console.log(`Hello, my name is ${this.name}`)}};
+//
+// let result = Two.sayHello.bind(One)
+//
+// // @ts-ignore
+// window.result = result
 
 // Task 06
 // создайте объект helperObj у которого есть следующие методы:
@@ -135,27 +206,215 @@ export {}
 // setAge - устанавливает полученное значение в свойство age объекта
 // greeting - используется функция sayHello из Task 05
 // можно использовать @ts-ignore
+//
+// let helperObj = {
+//
+//     name: '',
+//     age: 0,
+//     changeName(name: string){
+//         this.name = name
+//     },
+//     setAge(age:number){
+//         this.age = age
+//     },
+//     greeting: Two.sayHello
+// }
+//
+// // @ts-ignore
+// window.helperObj = helperObj
+//
+//
+
 
 // Bind
 // 1) Дана функция sumTwoNumbers, реализовать функцию bindNumber которая принимает функцию sumTwoNumbers и число, и
 // возвращает другую функцию, которое также принимает число и возвращает сумму этих чисел. Замыкание использовать нельзя
 // function sumTwoNumbers(a:number,b:number):number {return a + b};
 
+//
+// function sumTwoNumbers(a:number=10,b:number=10):number {return a + b}
+//
+// type sumTwoNumbersType = typeof sumTwoNumbers
+//
+// const bindNumber = (fn:sumTwoNumbersType, N:number) => {
+//
+//     return ()=>{
+//         return fn() + N
+//     }
+// }
+//
+// // @ts-ignore
+// window.sumTwoNumbers = sumTwoNumbers
+// // @ts-ignore
+// window.bindNumber = bindNumber
+
+
 // 2) Напишите функцию которая принимает первым аргументом объект One, а вторым helperObj. Данная функция
 // возвращает другую функцию которая принимает строку в качестве аргумента и устанавливает ее свойству name объекта One
+// type OneType = {
+//     name:string
+// }
+//
+// type helperObjType = {
+//     name:string
+//     age:number
+//     changeName: (name: string)=>void
+//     setAge: (age:number)=>void
+// }
+//
+// let One:OneType = {name: 'One'};
+//
+// let Two = {
+//     name: 'Two',
+//     sayHello: function() {console.log(`Hello, my name is ${this.name}`)}};
+//
+// let helperObj:helperObjType = {
+//     name: '',
+//     age: 0,
+//     changeName(name){
+//         this.name = name
+//     },
+//     setAge(age){
+//         this.age = age
+//     },
+// }
+
+//
+// // как переписать это на стрелочную функцию ?
+//  const someFunction = (obj:OneType, helperObj:helperObjType):any => {
+//
+// return  (newName:string)=>{
+//           helperObj.changeName.bind(obj,newName )
+//     }
+//
+//
+// }
+// someFunction(One,helperObj)('asdas')
+//
+//
+//     console.log(  One)
+// function atata(One: OneType, helperObj: helperObjType) {
+//     return function (nameN: string) {
+//         return helperObj.changeName.bind(One, nameN)
+//     }
+// }
+//
+// let hbca = atata(One, helperObj)
+// hbca('GGGGGGGGG')()
+// console.log(One)
+
+// @ts-ignore
+window.ONE = One
+
+// @ts-ignore
+window.helperObj =helperObj
+
+// @ts-ignore
+window.someFunction = someFunction
+
+
+
 // 3) Одной строкой установить с помощью helperObj объекту Two поле age в значение 30
+// window.helperObj.setAge.bind(window.ONE, 20)()
+
+
 // 4) Создать метод hi у объекта One, который всегда вызывает метод greeting объекта helperObj от имени Two
+// type OneType = {
+//     name:string
+//     hi: ()=>void
+// }
+//
+// type helperObjType = {
+//     name:string
+//     age:number
+//     changeName: (name: string)=>void
+//     setAge: (age:number)=>void
+//     greeting: ()=>void
+// }
+// let Two = {
+//     name: 'Two',
+//     sayHello: function() {
+//         console.log(`Hello, my name is ${this.name}`)
+//     }
+// };
+//
+//
+//
+// let helperObj:helperObjType = {
+//     name: '',
+//     age: 0,
+//     changeName(name){
+//         this.name = name
+//     },
+//     setAge(age){
+//         this.age = age
+//     },
+//     greeting: Two.sayHello
+// }
+//
+// let One:OneType = {
+//     name: 'One',
+//     hi: helperObj.greeting.bind(Two)
+// };
+//
+//
+//
+//
+// const someFunction = (obj2:OneType, helperObj:helperObjType) => {
+//
+//     let obj = obj2
+//
+//     let newFun = (newName:string)=>{
+//         helperObj.changeName(newName)
+//     }
+//     newFun('asdas')
+//
+//     return obj
+// }
+//
+// // @ts-ignore
+// window.ONE = One
+//
+// // @ts-ignore
+// window.helperObj =helperObj
+//
+// // @ts-ignore
+// window.someFunction = someFunction
+//
 
 // Реализовать задачи 2-4 из Bind с помощью Call
+/////////
+//     Что выведет функция?
+//     function f() {
+//         alert( this ); // ?
+//     }
+//
+//     let user = {
+//         g: f.bind(null)
+//     };
+//
+//     user.g();
+//     Ответ: null.
+//
+//         function f() {
+//         alert( this ); // null
+//     }
+//
+//     let user = {
+//         g: f.bind(null)
+//     };
+//
+//     user.g();
+//     Контекст связанной функции жёстко фиксирован. Изменить однажды привязанный контекст уже нельзя.
+//
+//         Так что хоть мы и вызываем user.g(), внутри исходная функция будет вызвана с this=null. Однако, функции g совершенно без разницы, какой this она получила. Её единственное предназначение – это передать вызов в f вместе с аргументами и ранее указанным контекстом null, что она и делает.
+//
+//         Таким образом, когда мы запускаем user.g(), исходная функция вызывается с this=null.
 
-
-
+    ///////////////////////////////
 // just a plug
 // export default () => {};
 
-
-
-export const HomeWork5 = () => {
     return (
         <h2>
            LESSON 5
